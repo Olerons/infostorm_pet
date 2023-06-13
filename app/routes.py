6180b5a5cdf3b56b71ee27ded8604d60ccfa5ec3
Olerons <ser1a@ya.ru>
2023-06-13 17:36:05 +0300

    base template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -13,5 +13,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Its OK')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title="Вход", form=form)
